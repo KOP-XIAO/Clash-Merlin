0d5502c4e01650172a9e3f4ce0c03d99ac3eb693
Shawn <45581750+KOP-XIAO@users.noreply.github.com>
2019-03-23 21:19:50 +0800

    Add files via upload

--- clash-iptable.sh
+++ clash-iptable.sh
@@ -1,4 +1,7 @@
+#Clash
 start-stop-daemon -S -b -x /jffs/.koolshare/clash -m -p /tmp/clash.pid -- -d /jffs/.koolshare/
+# ports redirect for clash except port 22 for ssh connection
 iptables -t nat -A PREROUTING -p tcp --dport 22 -j ACCEPT
 iptables -t nat -A PREROUTING -p tcp -j REDIRECT --to-ports 8887
+#iptables -t nat -A PREROUTING -p tcp -m tcp --dport 53 -j DNAT --to-destination 192.168.50.1:53
 iptables -t nat -A PREROUTING -p udp -m udp --dport 53 -j DNAT --to-destination 192.168.50.1:55
